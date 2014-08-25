869fb681a836f81f5c2f6e6347e786fc00fe42f5
Brian <boyen@mitake.com.tw>
2014-08-25 16:45:34 +0800

    indexeddb fix

--- po/app/js/idb/idb.js
+++ po/app/js/idb/idb.js
@@ -3,13 +3,14 @@ $(function(){
     init = function(){
 
         idb_timeline_events = new IDBStore({
-          dbVersion: 1,
+          dbVersion: 2,
           storeName: 'timeline_events',
           keyPath: 'ct_ei',
           indexes: [
             { name: 'gi'}
           ]
         });
+
     }
 
     //do it
