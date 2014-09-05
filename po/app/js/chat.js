9d12a546ad11da9fa1a2528d6bb1456861e70a27
Gloria Lin <gloria.lin@mitake.com.tw>
2014-09-05 14:02:28 +0800

    新增點擊跳出大圖功能, 修正聊天室列表無法捲動問題

--- po/app/js/chat.js
+++ po/app/js/chat.js
@@ -328,12 +328,24 @@ getS3file = function(target, file_c, file_p, tp, ti, size){
 						// 	scrollToBottom();
 						// }
 			        });
+			        
 					//小圖
 					img.attr("src",obj.s3);
-					// img.click( function(){
-					// 	var tmp = window.open("");
-					// 	tmp.$("body").append("<img src="+obj.s32+">");
-					// });
+					//點擊跳出大圖
+					img.click( function(){
+						var imgO = new Image();
+						var gallery_str = "<img src=" + obj.s32 + " />";
+						imgO.onload = function() {
+							var gallery = window.open("layout/chat_gallery.html", "", "width=" + (this.width+30) + ", height=" + (this.height+25));
+					    	$(gallery.document).ready(function(){
+								setTimeout(function(){
+									var this_slide = $(gallery.document).find(".pic");
+									this_slide.html( gallery_str );
+								},300);
+							});
+					    }
+						imgO.src = obj.s32;
+					});
 					break;
 				case 8://聲音
 					target.attr("src",obj.s3);
