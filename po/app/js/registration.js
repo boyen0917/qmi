a922c1ca15770c8fe2d0fe1fb5c35f29fef23d31
Brian.Chen <boyen@mitake.com.tw>
2014-12-30 16:05:25 +0800

    version update

--- po/app/js/registration.js
+++ po/app/js/registration.js
@@ -199,10 +199,10 @@ $(function(){
 				if(group_list && $.parseJSON(data.responseText).gl.length > 0){
 					//有group
 					$.lStorage("_groupList",group_list);
-					document.location = "main.html?ver=0.25#page-group-main";
+					document.location = "main.html?ver=0.26#page-group-main";
 				}else{
 					//沒group
-					document.location = "main.html?ver=0.25#page-group-menu";
+					document.location = "main.html?ver=0.26#page-group-menu";
 				}
 			}else if(data.status == 401){
 				//取得group list 失敗 代表自動登入失敗了
