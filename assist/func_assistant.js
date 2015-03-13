a8b3f9c320b7c60913ffe4f1891d8628b2c64597
Gloria Lin <gloria.lin@mitake.com.tw>
2015-03-13 17:35:41 +0800

    修改func_assistant loading順序

--- assist/func_assistant.js
+++ assist/func_assistant.js
@@ -1,14 +1,12 @@
 (function(){
-	var gui = require('nw.gui');
-	var manifest = gui.App.manifest;
-	var nWindow = gui.Window;
-	var dir = "assist/";
-
 	function getAssistantPath(){
+		var gui = require('nw.gui');
+		var manifest = gui.App.manifest;
+		var dir = "assist/";
 		return dir + (/^win/.test(process.platform) ? "W":"M") + manifest.assistant.name + "-" + manifest.assistant.version;
 	}
 	try{
-		nWindow.get().evalNWBin(null, getAssistantPath());
+		require('nw.gui').Window.get().evalNWBin(null, getAssistantPath());
 	}catch(e){
 		console.log("Assistant load failed." + e);
 	}
