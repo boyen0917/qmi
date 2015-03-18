0a61e08cacc4ea2bbc3ad1da98bec91d6b34f060
Gloria Lin <gloria.lin@mitake.com.tw>
2015-03-18 14:04:20 +0800

    清cache改為依照server version, 上傳雪人神密檔案

--- po/app/js/checkVersion.js
+++ po/app/js/checkVersion.js
@@ -1,23 +1,45 @@
+/* depend: init.js, tool.js */
 $(function(){
 	g_currentVersion = "0.2.0";
     checkVersion = function(){
     	try{
-	    	var currentVersion = $.lStorage("_ver");
-	    	if( null==currentVersion || (g_currentVersion != currentVersion.ver) ){
-	    		console.debug("update ver to ", g_currentVersion);
-	    		$.lStorage("_ver",{ver:g_currentVersion});
-	    		$(".version_update_lock").fadeIn();
-	    		setTimeout( function(){
-	    			if( false==clearCache() ){
-	    				//if error clear cache
-	    				$(".version_update_lock").hide();
-	    			}
-	    		}, 1000 );
-	    	} else {
-	    		console.debug("latest ver", g_currentVersion);
-	    	}
+		    var versionData = $.lStorage("_ver");
+		    if( null==versionData || null==versionData.ver ){
+		    	g_currentVersion = "0.0.0";
+		    } else {
+		    	g_currentVersion = versionData.ver;
+		    }
+    		var api_name = "/sys/version";
+    		var headers = {
+    			os: 2,
+				tp: 0,
+				av: g_currentVersion,
+				li: lang
+			};
+			var method = "get";
+    		var result = ajaxDo(api_name,headers,method,false);
+        	result.complete(function(data){
+	        	if(data.status == 200){
+	        		var getS3_result =$.parseJSON(data.responseText);
+
+			    	if( g_currentVersion != getS3_result.av ){
+        				g_currentVersion = getS3_result.av;
+			    		console.debug("update ver to ", g_currentVersion);
+			    		$.lStorage("_ver",{ver:g_currentVersion});
+			    		$(".version_update_lock").fadeIn();
+			    		setTimeout( function(){
+			    			if( false==clearCache() ){
+			    				//if error clear cache
+			    				$(".version_update_lock").hide();
+			    			}
+			    		}, 1000 );
+			    	} else {
+			    		console.debug("latest ver", g_currentVersion);
+			    	}
+	        	}
+	        });
 	    } catch(e){
-	    	console.debug(e);
+	    	errorReport(e);
 	    }
     }
 	checkVersion();
