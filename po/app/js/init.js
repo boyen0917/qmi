4b46d02d5432456fa8c257f32273206499943a58
Brian.Chen <boyen@mitake.com.tw>
2016-02-17 14:56:04 +0800

    gulp uglify and base_url to emome

--- po/app/js/init.js
+++ po/app/js/init.js
@@ -3,8 +3,8 @@ var ui,at,lang,gi;
 
  	gi = null;
  	//HiCloud
- 	base_url = "https://apserver.mitake.com.tw/apiv1/";
- 	// base_url = "https://ap.qmi.emome.net/apiv1/";
+ 	// base_url = "https://apserver.mitake.com.tw/apiv1/";
+ 	base_url = "https://ap.qmi.emome.net/apiv1/";
 
 	//local測試 預設開啟console
 	debug_flag = false;
