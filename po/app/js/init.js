a218cac783af79090f754c81db9ac66328924648
Brian.Chen <boyen@mitake.com.tw>
2016-03-30 14:45:44 +0800

    add url detection - qa and production

--- po/app/js/init.js
+++ po/app/js/init.js
@@ -3,13 +3,13 @@ var ui,at,lang,gi;
 
  	gi = null;
  	//HiCloud
- 	base_url = "https://qaap.qmi.emome.net/apiv1/";
+ 	base_url = "https://ap.qmi.emome.net/apiv1/";
 
 	//local測試 預設開啟console
 	debug_flag = false;
-	if(window.location.href.match(/^http:\/\/localhost|10.1.17.114/)) {
+	if(window.location.href.match(/^https:\/\/qawp.qmi.emome.net/)) {
 		debug_flag = true;
-		// base_url = "https://capubliceim.mitake.com.tw/apiv1/";
+		base_url = "https://qaap.qmi.emome.net/apiv1/";
 		// base_url = "https://apserver.mitake.com.tw/apiv1/";
 	}
 	
