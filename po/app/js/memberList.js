23a9fc168e3153abb40cae2606b4fc281f551a40
MITAKE\sam <sam@mitake.com.tw>
2017-03-22 11:52:49 +0800

    聊天室顯示成員頁面優化

--- po/app/js/memberList.js
+++ po/app/js/memberList.js
@@ -49,7 +49,7 @@ showMemListPageDelegate = function( title, list, onPageChanged, onDone ){
 
     //generate page when click
     tabArea.next().html("");
-    $(document).find("#page-select-object .tab").click(function(){
+    page.find(".tab").click(function(){
         // $(window).scrollTop(20);
         $("body").addClass("user-info-adjust");
         setTimeout(function(){
@@ -59,28 +59,30 @@ showMemListPageDelegate = function( title, list, onPageChanged, onDone ){
         var tab = $(this);
         var index = tab.data("id");
         var cell = cellArea.find("._"+index);
+        var listData = tab.data("obj").ml;
+        var userData = $.userStorage();
+        var guAll = userData[gi].guAll;
+        var bl = userData[gi].bl;
 
         $("#page-select-object .tab").removeClass("current");
         tab.addClass("current");
 
-        if( cell.length<=0 ){
-            var data = tab.data("obj");
-            cell = $("<div class='obj-cell-page _"+index+"'></div>");
-            cellArea.append( cell );
 
-            //gen mem
-            var userData = $.userStorage();
-            var guAll = userData[gi].guAll;
-            var bl = userData[gi].bl;
-            for(var i=0;i<data.ml.length; i++ ){
-                var gu = data.ml[i].gu;
-                var rt = data.ml[i].rt;
-                if( !gu ) continue;
-                if( !guAll.hasOwnProperty(gu) ) continue;
+        var makeMemberList = function () {
+            var currentMembum = cellArea.find("._" + index + " .obj-cell").length;
+            var loadMemList = listData.slice(currentMembum, currentMembum + 100);
+
+            if (currentMembum + 100 > listData.length - 1) {
+                loadMemList = listData.slice(currentMembum);
+            } 
+
+            loadMemList.forEach(function (member) {
+                var gu = member.gu;
+                var rt = member.rt;
                 var mem = guAll[gu];
                 var this_obj = $(
                     '<div class="obj-cell mem" data-gu="'+gu+'">' +
-                        '<div class="obj-cell-user-pic namecard"><img src="images/common/others/empty_img_personal_xl.png" style="width:60px"/></div>' +
+                        '<div class="obj-cell-user-pic"><img src="images/common/others/empty_img_personal_xl.png" style="width:60px"/></div>' +
                         '<div class="obj-cell-time"></div>' +
                         '<div class="obj-cell-user-data">' + 
                             '<div class="obj-user-name">' + mem.nk.replaceOriEmojiCode() + '</div>' +
@@ -90,9 +92,9 @@ showMemListPageDelegate = function( title, list, onPageChanged, onDone ){
 
                 var branchID = mem.bl;
                 var extraContent = "";  //mem.em;
-                if( branchID && branchID.length>0 ){
+                if( branchID && branchID.length > 0 ){
                     var branchPath = branchID.split(".");
-                    if( branchPath && branchPath.length>0 ){
+                    if( branchPath && branchPath.length > 0 ){
                         branchID = branchPath[branchPath.length-1];
                         if( bl.hasOwnProperty(branchID) ){
                             extraContent = bl[branchID].bn;
@@ -117,11 +119,80 @@ showMemListPageDelegate = function( title, list, onPageChanged, onDone ){
 
                 this_obj.data("gu",mem.gu);
                 this_obj.data("gu-name",mem.nk);
-                cell.append(this_obj);
-            }
+                cellArea.find("._"+index).append(this_obj);
+            });
         }
-        $("#page-select-object .obj-cell-page.current").hide().removeClass("current");
+
+        if( cell.length<=0 ){
+            // var data = tab.data("obj");
+            cell = $("<div class='obj-cell-page _"+index+"'></div>");
+            cellArea.append( cell );
+
+            makeMemberList();
+
+            //gen mem
+            // var userData = $.userStorage();
+            // var guAll = userData[gi].guAll;
+            // var bl = userData[gi].bl;
+            // for(var i=0;i<data.ml.length; i++ ){
+            //     var gu = data.ml[i].gu;
+            //     var rt = data.ml[i].rt;
+            //     if( !gu ) continue;
+            //     if( !guAll.hasOwnProperty(gu) ) continue;
+            //     var mem = guAll[gu];
+            //     var this_obj = $(
+            //         '<div class="obj-cell mem" data-gu="'+gu+'">' +
+            //             '<div class="obj-cell-user-pic namecard"><img src="images/common/others/empty_img_personal_xl.png" style="width:60px"/></div>' +
+            //             '<div class="obj-cell-time"></div>' +
+            //             '<div class="obj-cell-user-data">' + 
+            //                 '<div class="obj-user-name">' + mem.nk.replaceOriEmojiCode() + '</div>' +
+            //                 '<div class="obj-user-title"></div>' +
+            //         '</div>'
+            //     );
+
+            //     var branchID = mem.bl;
+            //     var extraContent = "";  //mem.em;
+            //     if( branchID && branchID.length>0 ){
+            //         var branchPath = branchID.split(".");
+            //         if( branchPath && branchPath.length>0 ){
+            //             branchID = branchPath[branchPath.length-1];
+            //             if( bl.hasOwnProperty(branchID) ){
+            //                 extraContent = bl[branchID].bn;
+            //             }
+            //         }
+            //     }
+            //     if(extraContent && extraContent.length>0){
+            //         this_obj.find(".obj-cell-user-data").addClass("extra");
+            //         this_obj.find(".obj-user-title").html( extraContent );
+            //     }
+
+            //     var object_img = this_obj.find(".obj-cell-user-pic img");
+            //     if(mem.aut) {
+            //         object_img.attr("src",mem.aut);
+            //         //object_img.removeAttr("style");
+            //         // avatarPos(object_img);
+            //     }
+            //     if( rt ) {
+            //         this_obj.find(".obj-cell-time").html( new Date(rt).toFormatString() );
+            //     }
+            //     this_obj.find(".obj-cell-user-pic.namecard").data("gu",mem.gu);
+
+            //     this_obj.data("gu",mem.gu);
+            //     this_obj.data("gu-name",mem.nk);
+            //     cell.append(this_obj);
+            // }
+        }
+        page.find(".obj-cell-page.current").hide().removeClass("current");
         cell.show().addClass("current");
+
+        page.find(".obj-cell-page.current").off("scroll").on("scroll", function (e) {
+            var container = $(e.target);
+            if (container.scrollTop() + container.height() > container[0].scrollHeight - 20) {
+                if (container.find(" .obj-cell").length < listData.length) {
+                    makeMemberList();
+                }
+            }
+        });
     }); 
     tabArea.find(".tab:nth-child(1)").trigger("click");
 
