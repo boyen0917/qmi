86a39192eba82d397f8190d0849833e9edab6acd
Brian.Chen <boyen@mitake.com.tw>
2017-08-15 12:00:21 +0800

    1.8.4.1

--- po/app/js/memberTabList.js
+++ po/app/js/memberTabList.js
@@ -22,7 +22,7 @@ loadObjectTabPage = function( parentDom, onDone ){
     if( $("#page-tab-object").length>0 ){
         if( onDone ) onDone();
     } else {
-        $('<div>').load('layout/memberList.html?v1.8.4.0',function(){
+        $('<div>').load('layout/memberList.html?v1.8.4.1',function(){
             parentDom.after( $(this).find("#page-tab-object") );
             if( onDone ) onDone();
         });
