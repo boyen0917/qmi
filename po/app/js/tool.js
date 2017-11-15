2e767760cb98006e78a29941f2104eef21272053
MITAKE\sam <sam@mitake.com.tw>
2017-11-15 18:51:08 +0800

    網址加上中文的判斷

--- po/app/js/tool.js
+++ po/app/js/tool.js
@@ -389,7 +389,7 @@ QmiGlobal.aesCrypto = {
 }
 
 htmlFormat = function (str, isToCharCode){
-	var urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+	var urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-\u00BF-\u1FFF\u2C00-\uD7FF_a-zA-Z0-9@:%_\+.~#?&//=]*)/g
 	if(str.match(/\&\#\d+\;*/g)){
 		str = str.replace(/\&\#/g,"&#38;&#35;");
   	} 
