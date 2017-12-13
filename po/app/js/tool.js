22bece1bd89276cf7127fdf72f34e66911680638
Brian.Chen <boyen@mitake.com.tw>
2017-12-13 17:09:23 +0800

    刪除console log

--- po/app/js/tool.js
+++ po/app/js/tool.js
@@ -393,7 +393,6 @@ htmlFormat = function (str, isToCharCode){
 		urlPattern = "(http(s)?:\\/\\/.)?(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.(?:(?:" + tlds + ")(?=[^0-9a-zA-Z@]|$))\\b([-\u00BF-\u1FFF\u2C00-\uD7ff_a-zA-Z0-9@:%_\\+.~#?&//=]*)",
 		urlRegex = new RegExp(urlPattern, 'gi');
 
-	console.log(urlPattern);
 	if(str.match(/\&\#\d+\;*/g)){
 		str = str.replace(/\&\#/g,"&#38;&#35;");
   	} 
@@ -402,7 +401,6 @@ htmlFormat = function (str, isToCharCode){
     	var newStr = (isToCharCode === true ? encodeHtmlEntity(val) : val) ;
     	
     	if(val.match(urlRegex)) {
-    		console.log('ewfwe')
 			newStr = val.replace(urlRegex, function (match) {
 				if (match.substring(0, 7) == 'http://' || match.substring(0, 8) == 'https://') {
 					return "<a href=\"" + match + "\" target=\"_blank\">" + match + "</a>";
@@ -805,7 +803,6 @@ qmiUploadS3 = function(uploadObj,s3Obj) {
 		mediaLoadDef.done(chainDef.resolve);
 		return chainDef;
 	}()).then(function() {
-		console.log(paramObj.s3.file)
 		$.when.apply($, Object.keys(paramObj).reduce(function(arr,key,i) {
 
 			var ajaxArgs = {
@@ -2256,7 +2253,6 @@ zipVideoFile = function (videoObj) {
    			.videoCodec('libx264')
    			.audioCodec('aac')
 
-   			console.log(isVerticalVideo)
    			if (isVerticalVideo) {
    				command.outputOptions("-vf scale=360:640,setsar=1")
    			} else {
@@ -2471,7 +2467,6 @@ QmiGlobal.MemberLocateModal = function (data, thisTimeline) {
 		    		liElement.attr("data-gu", taskFinisher.meta.gu);
 		    		// 點擊頭像跳出個人主頁視窗
 		    		liElement.find("img").off("click").on("click", function(e) {
-		    			console.log( $(e.target).parent().attr("data-gu"));
 		    			var target = $(e.target);
 		    			userInfoShow(gi, target.parent().attr("data-gu"));
 		    		});
