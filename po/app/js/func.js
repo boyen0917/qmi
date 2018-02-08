6e862f2a5a3581b379f24496a1833d6770f270cf
MITAKE\sam <sam@mitake.com.tw>
2018-02-08 10:38:57 +0800

    新增對象無法取消舊的發布對象

--- po/app/js/func.js
+++ po/app/js/func.js
@@ -2671,6 +2671,8 @@ composeObjectShow = function(this_compose){
 }
 
 composeObjectShowDelegate = function( thisCompose, thisComposeObj, option, onDone ){
+    console.log(thisCompose);
+    console.log(thisCompose.data("object_str"))
     var objectDelegateView = ObjectDelegateView;
     var objData, branchData, favoriteData;
     var isShowBranch = false;
@@ -2681,6 +2683,7 @@ composeObjectShowDelegate = function( thisCompose, thisComposeObj, option, onDon
     var isBack = true;
     var isShowLeftMem = false;
     var isForWork = thisComposeObj.parent().hasClass("cp-work-item");
+    var isDisableOnAlreadyChecked = false;
 
     var group = QmiGlobal.groups[gi],
         guAll = group.guAll,
@@ -2713,6 +2716,7 @@ composeObjectShowDelegate = function( thisCompose, thisComposeObj, option, onDon
         isShowFavBranch = (null==option.isShowFavBranch) ? isShowFavBranch : option.isShowFavBranch;
         isBack = (null==option.isBack) ? isBack : option.isBack;
         isShowLeftMem = (null==option.isShowLeftMem) ? isShowLeftMem : option.isShowLeftMem;
+        isDisableOnAlreadyChecked = (null==option.isDisableOnAlreadyChecked) ? isDisableOnAlreadyChecked : option.isDisableOnAlreadyChecked
     }
 
     if (thisComposeObj.parent().hasClass("cp-work-item")) {
@@ -2763,6 +2767,8 @@ composeObjectShowDelegate = function( thisCompose, thisComposeObj, option, onDon
         checkedBranches : branchData,
         checkedFavorites : favoriteData,
         minSelectNum : 0,
+        isDisableOnAlreadyChecked : isDisableOnAlreadyChecked
+
     }
 
     objectDelegateView.init(viewOption).setHeight();
@@ -2781,7 +2787,8 @@ composeObjectShowDelegate = function( thisCompose, thisComposeObj, option, onDon
         objectDelegateView.addRowElement("Favorite");
 
         visibleMemList.forEach(function(gu) {
-            var guObj = guAll[gu];
+            var guObj = Object.assign({}, guAll[gu]);
+            if (objData.hasOwnProperty(gu)) guObj.chk = true;
             if (guObj.fav) {
                 objectDelegateView.addFavoriteSubRow("Member", {thisMember : guObj, isSubRow : true});
             }
@@ -5148,6 +5155,12 @@ eventStatusWrite = function(this_event,this_es_obj){
         this_event.find(".st-sub-box-more .st-sub-box-more-box[data-st-more='del']").removeClass("deactive");
     } else {
         this_event.find(".st-sub-box-more .st-sub-box-more-box[data-st-more='del']").addClass("deactive");
+    }
+
+    if( this_es_obj.hasOwnProperty("tu") ){
+        this_event.find(".st-sub-box-more .st-sub-box-more-box[data-st-more='object']").removeClass("deactive");
+    } else {
+        this_event.find(".st-sub-box-more .st-sub-box-more-box[data-st-more='object']").addClass("deactive");
     }
 }
 
