bf1dec57e19f3cf16818098bc73fe2e7166bfced
Brian.Chen <boyen@mitake.com.tw>
2018-03-07 14:05:42 +0800

    2.2.0.3

--- po/app/js/init.js
+++ po/app/js/init.js
@@ -1,4 +1,4 @@
-// version 2.2.0.2
+// version 2.2.0.3
 var ui;
 var at;
 var gi;
@@ -51,7 +51,7 @@ window.QmiGlobal = {
 	// 這是web版號 另有桌機版號 module.js deskTopVersion
 	// 多加一個條件: 若桌機版號大於web版號 以桌機版號為主
 	// initReady裡面做調整
-	appVer: "2.2.0.2",
+	appVer: "2.2.0.3",
 
 	title: "Qmi",
 
